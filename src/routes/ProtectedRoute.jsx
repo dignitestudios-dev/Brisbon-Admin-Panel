3b748bf0d9e69ee6587b913471641fb7b2c1eb24
evansmith14 <184949993+evansmith14@users.noreply.github.com>
2025-05-22 03:58:05 -0700

    removed access denied msg from protectedroutes

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,3 +1,28 @@
+// // src/components/ProtectedRoute.jsx
+// import { Navigate } from "react-router";
+// import Cookies from "js-cookie";
+// import { ErrorToast } from "../components/global/Toaster";
+
+// function ProtectedRoute({ children }) {
+//   const token = Cookies.get("token");
+
+//   if (!token) {
+//     const errorHandler = (msg) => {
+//       ErrorToast(msg);
+//       return
+//     }
+//     return (
+//       <div className="flex flex-col items-center justify-center h-screen">
+//         {errorHandler("Access Denied! Please login first.")}
+//         <Navigate to="/auth/login" replace />
+//       </div>
+//     );
+//   }
+
+//   return children;
+// }
+
+// export default ProtectedRoute;
 // src/components/ProtectedRoute.jsx
 import { Navigate } from "react-router";
 import Cookies from "js-cookie";
